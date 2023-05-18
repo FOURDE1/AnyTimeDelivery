8f605c82dc35f0364bd73e0f9bd970d9f4ade74e
AbbasMou <10121021@mu.edu.lb>
2023-05-18 03:07:59 +0300

    commit

--- javascript/registerJs.js
+++ javascript/registerJs.js
@@ -1,57 +1,77 @@
 var menuHolder = document.getElementById('menuHolder')
 var siteBrand = document.getElementById('siteBrand')
-function menuToggle(){
-  if(menuHolder.className === "drawMenu") menuHolder.className = ""
+function menuToggle() {
+  if (menuHolder.className === "drawMenu") menuHolder.className = ""
   else menuHolder.className = "drawMenu"
-}
-if(window.innerWidth < 426) siteBrand.innerHTML = "MAS"
-window.onresize = function(){
-  if(window.innerWidth < 420) siteBrand.innerHTML = "MAS"
-  else siteBrand.innerHTML = "MY AWESOME WEBSITE"
 }
 
 
 // animation of logo 
 
 function myFunction() {
-    var img = document.getElementById("my-image");
-    img.classList.add("slide-animation");
-  }
+  var img = document.getElementById("my-image");
+  img.classList.add("slide-animation");
+}
 
 
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('click', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
-  window.onload = function () {
-    document.getElementById("password1").onchange = validatePassword;
-    document.getElementById("password2").onchange = validatePassword;
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('click', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+// window.onload = function () {
+//   document.getElementById("password1").onchange = validatePassword;
+//   document.getElementById("password2").onchange = validatePassword;
+// }
+// function validatePassword() {
+//   var pass2 = document.getElementById("password2").value;
+//   var pass1 = document.getElementById("password1").value;
+//   if (pass1 =="")
+//     document.getElementById("password2").setCustomValidity("Passwords Don't Match");
+//     else if(pass1 != pass2){
+//       document.getElementById("password1").setCustomValidity("cannot be empty");
+//     }
+//   else
+//     document.getElementById("password2").setCustomValidity('');
+//   //empty string means no validation error
+// }
+document.addEventListener("DOMContentLoaded", function () {
+  var password1 = document.getElementById("password1");
+  var password2 = document.getElementById("password2");
+  var passwordErrorMessage = document.querySelector(".invalid-feedback");
+
+  function checkPasswordNotEmpty() {
+    if (password1.value === "") {
+      passwordErrorMessage.textContent = "Please enter a password.";
+    } else {
+      passwordErrorMessage.textContent = ""; // Clear any previous error message
+    }
   }
-  function validatePassword() {
-    var pass2 = document.getElementById("password2").value;
-    var pass1 = document.getElementById("password1").value;
-    if (pass1 =="")
-      document.getElementById("password2").setCustomValidity("Passwords Don't Match");
-      else if(pass1 != pass2){
-        document.getElementById("password1").setCustomValidity("cannot be empty");
-      }
-    else
-      document.getElementById("password2").setCustomValidity('');
-    //empty string means no validation error
+
+  function checkPasswordMatch() {
+    if (password1.value !== password2.value) {
+      passwordErrorMessage.textContent = "Passwords do not match.";
+    } else {
+      passwordErrorMessage.textContent = ""; // Clear any previous error message
+    }
   }
+
+  password1.addEventListener("input", checkPasswordNotEmpty);
+  password2.addEventListener("input", checkPasswordMatch);
+});
+
