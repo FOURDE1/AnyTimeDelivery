3ed4f5860571620ed55c4811fb7b686fd3c1020c
AbbasMou <10121021@mu.edu.lb>
2023-05-31 03:36:10 +0300

    sae as last commit

---
+++ DeliverySite/Migrations/20230530221334_addingTakenOrder.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeliverySite.Migrations
+{
+    /// <inheritdoc />
+    public partial class addingTakenOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsTaken",
+                table: "Orders",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsTaken",
+                table: "Orders");
+        }
+    }
+}
---
+++ DeliverySite/Migrations/20230530231058_AddingDeliveryToOrder.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeliverySite.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingDeliveryToOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DeliveryId",
+                table: "Orders",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeliveryId",
+                table: "Orders");
+        }
+    }
+}
